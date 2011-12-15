782f343a317e94a93df362464b4d1937cad83419
kamarush <kamarush@windowslive.com>
2011-12-15 21:06:06 +0100

    wlan modules

---
+++ ap/platforms/os/linux/msm_env.bash
@@ -0,0 +1,4 @@
+export CROSS_COMPILE=/home/kamarush/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export ARCH=arm
+export HOST_PLATFORM=msm
+export KERNEL_DIR=/home/kamarush/Xperia-2011-KRsH-Kernel-2.6.32.9
